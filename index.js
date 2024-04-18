72b78c29a5d1737d3075d7759edfbaa285f6fb29
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-18 11:15:50 +0300

    Add 'welcomUser'

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-//adult or not adult
 
-/**
- * 
- * @param {number} age
- * @return {string} 
- */
-function isAdultUser(age){
-    return age >= 18;
+
+//write 'thanks' if user wrote smth or write'error'
+
+function welcomUser(){
+    const keyWorld = prompt();
+    return  keyWorld === null || keyWorld === '' ? 'error' :'thanks';
 }
 
 
-/**
- * 
- * @param {number} age
- * @return {string} 
- */
-function getStatusAdultUser(age){
-    // if (age >= 18) {
-    //     return 'adult';
-    // }
-    // return 'not adult';
-    return age >= 18 ? 'adult' : 'not adultd';
-}
