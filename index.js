54ba0fe76e3b1504e373afcfb3e21dbdb0913d4c
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-19 10:24:53 +0300

    Add while operator

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 
-const monthUser = prompt('month');
-const month = Number(monthUser)
-//
-if(
-    month<1 || 
-    month>12 ||
-    Number.isNumber(month) || 
-    Number.isInteger(number) === false
-  ) {
-  console.log('wrong data')
-} else if(month<=2 || month===12){
-  console.log('wnter');
-} else if(month <= 5){
-  console.log('spring')
-} else if(month === 6 || month === 7 || month === 8) {
-    console.log('summer')
-} else if (month>8) {
-    console.log('autumn');
-} else {
-    console.log('error')
+//loop
+
+// let count = 0;
+// while (count<3) {
+//     count++;
+//     console.log(count);
+// }
+
+
+
+count = 10;
+while (true) {
+    count++;
+    console.log(count);
+    if(count >= MAX_VALUE) break;
 }
-
-
-
