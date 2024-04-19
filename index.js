781256376204730ef86dbce9d44406d8c37b1e29
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-19 09:45:23 +0300

    Add exampl of if

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 
-
-//write 'thanks' if user wrote smth or write'error'
-
-function welcomUser(){
-    const keyWorld = prompt();
-    return  keyWorld === null || keyWorld === '' ? 'error' :'thanks';
+const monthUser = prompt('month');
+const month = Number(monthUser)
+//
+if(
+    month<1 || 
+    month>12 ||
+    Number.isNumber(month) || 
+    Number.isInteger(number) === false
+  ) {
+  console.log('wrong data')
+} else if(month<=2 || month===12){
+  console.log('wnter');
+} else if(month <= 5){
+  console.log('spring')
+} else if(month === 6 || month === 7 || month === 8) {
+    console.log('summer')
+} else if (month>8) {
+    console.log('autumn');
+} else {
+    console.log('error')
 }
+
 
 
