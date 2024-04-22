96c2542821ae3d350e010ebb8a7a67b80ab74298
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-22 10:21:51 +0300

    Add 'object'

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 
-const VALID_PASSWORD = 'qwerty'
 
-while (true) {
-  const userInputPassword = promt('Enter password');
-  if(userInputPassword === VALID_PASSWORD) {
-    console.log('welcome');
-    break;
-  }
-}
+const object1 = new Oblect();
+object1.prop1 = 'prop1'
+const object2 = bjetct();
 
-console.log(userInputPassword);
+//
+const object = {};
+
+const button = {
+  //properties
+  bgcolor: 'red'
+};
+
+button.width = 50;
+
+
+console.log(button);
