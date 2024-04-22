d1d1d1b46f9bad5c936a9c540176fd6898929058
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-22 11:39:45 +0300

    Add 'this.---' example

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 
-let user = {
-  fname: 'Brad',
-  sname: 'Pitt'
+// const user = {
+//   fname: 'Brad',
+//   sname: 'Pitt'
+
+// }
+
+// console.log(user);
+
+// user.age = 60;
+// user.isAdult = true;
+
+// console.log(user.age);
+
+
+// const user2 = {
+//   fname: 'Tom',
+//   sname: 'Rot',
+//   password: 'qwerty',
+//   age: 59,
+//   isAdult: true,
+//   sayFirstName: function(){
+//     return 'Hi, i\'m ' + user.fname;
+//   },
+//   saySecondName: function(){
+//     return `Hi, i\'m  + ${user.sname}`;
+//   }
+// };
+// console.log(user);
+
+//
+function User(fnameValue, sname, password, age, isAdult){
+  this.fname = fnameValue;
+  this.sname = sname;
+  this.password = password;
+  this.age = age;
+  this.isAdult = isAdult;
+  this.sayFullName = function () {
+    return `${this.fname} ${this.ansme}`;
+  };
+  this.getBdYear = function () {
+    //body function
+    return 1963;
+  };
+  this.showSubscribe = function () {
+    return 'you';
+  }
+
 }
 
-const activeuser = user;
-activeuser.age = 60;
-
-const userClient = activeUser;
-userClient.order = '123';
+const user2 = new User('Brad','Pitt','qwerty',60,true);
+console.log(user2);
